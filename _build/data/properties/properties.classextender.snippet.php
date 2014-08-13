fc7794b5635aaf08b3bd41b4d133bcc6c7018559
bobray <bobray@softville.com>
2014-08-12 23:54:10 -0500

    Remove all properties from ClassExtender snippet except tablePrefix

--- _build/data/properties/properties.classextender.snippet.php
+++ _build/data/properties/properties.classextender.snippet.php
@@ -13,66 +13,6 @@
 
 
 $properties = array (
-  'class' => 
-  array (
-    'name' => 'class',
-    'desc' => 'ce.class_desc',
-    'type' => 'textfield',
-    'options' => 
-    array (
-    ),
-    'value' => '',
-    'lexicon' => 'classextender:properties',
-    'area' => '',
-  ),
-  'method' => 
-  array (
-    'name' => 'method',
-    'desc' => 'ce.method_desc',
-    'type' => 'textfield',
-    'options' => 
-    array (
-    ),
-    'value' => 'use_schema',
-    'lexicon' => 'classextender:properties',
-    'area' => '',
-  ),
-  'package' => 
-  array (
-    'name' => 'package',
-    'desc' => 'ce.package_desc',
-    'type' => 'textfield',
-    'options' => 
-    array (
-    ),
-    'value' => '',
-    'lexicon' => 'classextender:properties',
-    'area' => '',
-  ),
-  'parentObject' => 
-  array (
-    'name' => 'parentObject',
-    'desc' => 'ce.parent_object_desc',
-    'type' => 'textfield',
-    'options' => 
-    array (
-    ),
-    'value' => '',
-    'lexicon' => 'classextender:properties',
-    'area' => '',
-  ),
-  'tableName' => 
-  array (
-    'name' => 'tableName',
-    'desc' => 'ce.table_name_desc',
-    'type' => 'textfield',
-    'options' => 
-    array (
-    ),
-    'value' => '',
-    'lexicon' => 'classextender:properties',
-    'area' => '',
-  ),
   'tablePrefix' => 
   array (
     'name' => 'tablePrefix',
