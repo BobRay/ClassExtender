e00d98c5d2d0d30973adb0d8883eb8e521f3b40e
bobray <bobray@softville.com>
2023-03-27 23:31:10 -0500

    Refactor autoloader to prevent loading wrong directory; suppress E_NOTICE errors

--- core/components/classextender/model/ce_autoload.php
+++ core/components/classextender/model/ce_autoload.php
@@ -1,41 +1,70 @@
 <?php
 
-/* MODX 2 provides an include in the classes
-   so this needs require_once */
 function ce_autoload($class) {
     global $modx;
-    $modelPath = $modx->getOption('core_path') .
+    $debug = true;
+    $corePath = $modx->getOption('core_path');
+    $modx->log(modX::LOG_LEVEL_ERROR, 'Class: ' . $class);
+
+
+    $modelPath = $corePath .
         'components/classextender/model/';
-
-    /* Get directories from System Setting
-       and convert to array */
-    $dirString = $modx->getOption('ce_autoload_directories', null,
-        'extendeduser,extendedresource', true);
-
-    $directories = explode(',', $dirString);
 
     if (strpos($class, '\\') !== false) {
         /* Namespaced */
-        $file = basename($class);
-    } else {
-        /* non-namespaced */
-        $file = $class;
-    }
+        /* $filePath includes class name */
+        $filePath = str_replace('\\', '/', $class);
 
-    foreach ($directories as $dir) {
-        $dir .= '/';
+
+        $path = $modelPath . $filePath . '.php';
+        $modx->log(modX::LOG_LEVEL_ERROR, 'Path: ' . $path);
+        if (@is_readable($path)) {
+            require $path;
+            if ($debug) {
+                $modx->log(modX::LOG_LEVEL_ERROR, 'Found');
+            }
+            return;
+        }
+        $path = $modelPath . $filePath . '.class.php';
+        if ($debug) {
+            $modx->log(modX::LOG_LEVEL_ERROR, 'Path: ' . $path);
+        }
+        if (@is_readable($path)) {
+            require $path;
+            return;
+        }
+
+    } else { /* non-namespaced */
+
+        /* Get directories from System Setting
+       and convert to array */
+        $dirString = $modx->getOption('ce_autoload_directories', null,
+            'extendeduser,extendedresource', true);
+
+        $directories = explode(',', $dirString);
+
+        $file = $class;
         $paths = array(
-            $modelPath . $dir . 'mysql/' . $file . '.class.php',
-            $modelPath . $dir . 'mysql/' . $file . '.php',
-            $modelPath . $dir . $file . '.php',
-            $modelPath . $dir . $file . '.class.php',
+            '/mysql/' . $file . '.class.php',
+            '/mysql/' . $file . '.php',
         );
 
-        foreach ($paths as $path) {
-            if (@is_readable($path)) {
-                require_once $path;
-                /* Stop looking */
-                break 2;
+        foreach ($directories as $dir) {
+
+            $paths = array(
+                $modelPath . $dir . '/mysql/' . $file . '.class.php',
+                $modelPath . $dir . '/mysql/' . $file . '.php',
+            );
+
+            foreach ($paths as $path) {
+                if (@is_readable($path))
+                $modx->log(modX::LOG_LEVEL_ERROR, 'Path: ' . $path);
+                if (@is_readable($path)) {
+                    $modx->log(modX::LOG_LEVEL_ERROR, 'Found');
+                    require_once $path;
+                    /* Stop looking */
+                    return;
+                }
             }
         }
     }
